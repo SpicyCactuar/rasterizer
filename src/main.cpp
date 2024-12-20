5ad48c84404840e28541053a7db727b3acd56fc2
SpicyCactuar <lamelaemanuel@gmail.com>
2024-12-19 21:23:04 -0300

    Fix frame time

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,21 @@
 
 int main(int argc, char* argv[]) try {
     static constexpr std::string_view title = "Hello Rasterizer";
+    static constexpr std::uint32_t FPS = 60;
+    static constexpr std::uint32_t FRAME_TIME = 1000 / FPS;
 
     const rasterizer::Scene scene;
     rasterizer::Application app(title, scene);
 
     while (app.isRunning) {
+        const std::uint32_t frameStart = SDL_GetTicks();
         app.processInput();
         app.update();
         app.render();
+        if (const std::uint32_t pendingFrameTime = SDL_GetTicks() - frameStart;
+            pendingFrameTime < FRAME_TIME) {
+            SDL_Delay(FRAME_TIME - pendingFrameTime);
+        }
     }
 
     return EXIT_SUCCESS;
