94506a5e5e7ceb33f22d3279cc3a19f2e2034668
SpicyCactuar <lamelaemanuel@gmail.com>
2024-12-29 17:18:21 -0300

    fix(canvas): operate on glm::ivec2, prevent precision issues

--- src/canvas.hpp
+++ src/canvas.hpp
@@ -31,38 +31,38 @@ namespace rasterizer {
             return buffer;
         }
 
-        void drawPixel(const std::uint32_t row, const std::uint32_t column, const std::uint32_t& color) const {
-            if (row < height && column < width) {
+        void drawPixel(const std::int32_t row, const std::int32_t column, const std::uint32_t& color) const {
+            if (0 <= row && row < height && 0 <= column && column < width) {
                 buffer[row * width + column] = color;
             }
         }
 
-        void drawRectangle(const std::uint32_t x, const std::uint32_t y,
+        void drawRectangle(const std::int32_t x, const std::int32_t y,
                            const std::uint32_t width, const std::uint32_t height,
                            const std::uint32_t color) const {
             const std::uint32_t endX = std::min(x + width, this->width);
             const std::uint32_t endY = std::min(y + height, this->height);
 
-            for (std::uint32_t row = y; row < endY; ++row) {
-                for (std::uint32_t column = x; column < endX; ++column) {
+            for (std::uint32_t row = std::max(y, 0); row < endY; ++row) {
+                for (std::uint32_t column = std::max(x, 0); column < endX; ++column) {
                     drawPixel(row, column, color);
                 }
             }
         }
 
-        void drawPoint(const glm::vec2& point) const {
+        void drawPoint(const glm::ivec2& point) const {
             static constexpr std::uint32_t rectangleColor = 0xFF7C3AED;
             static constexpr std::uint32_t pointWidth = 10, pointHeight = 10;
             // Draw centered, with side length 10
-            drawRectangle(static_cast<std::uint32_t>(point.x) - pointWidth / 2,
-                          static_cast<std::uint32_t>(point.y) - pointHeight / 2,
+            drawRectangle(point.x - static_cast<std::int32_t>(pointWidth / 2),
+                          point.y - static_cast<std::int32_t>(pointHeight / 2),
                           pointWidth, pointHeight, rectangleColor);
         }
 
-        void drawLine(const glm::vec2& start, const glm::vec2& end, const std::uint32_t lineColor) const {
+        void drawLine(const glm::ivec2& start, const glm::ivec2& end, const std::uint32_t lineColor) const {
             // DDA line rasterizer
-            const std::int32_t dx = static_cast<std::int32_t>(end.x) - static_cast<std::int32_t>(start.x);
-            const std::int32_t dy = static_cast<std::int32_t>(end.y) - static_cast<std::int32_t>(start.y);
+            const std::int32_t dx = end.x - start.x;
+            const std::int32_t dy = end.y - start.y;
 
             const std::uint32_t longestLength = std::abs(dx) >= std::abs(dy) ? std::abs(dx) : std::abs(dy);
 
@@ -73,14 +73,14 @@ namespace rasterizer {
             glm::float32_t x = start.x;
             glm::float32_t y = start.y;
             for (std::uint32_t l = 0; l <= longestLength; l++) {
-                drawPixel(static_cast<std::uint32_t>(std::round(y)), static_cast<std::uint32_t>(std::round(x)),
+                drawPixel(static_cast<std::int32_t>(std::round(y)), static_cast<std::int32_t>(std::round(x)),
                           lineColor);
                 x += xIncrement;
                 y += yIncrement;
             }
         }
 
-        void drawTriangle(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2) const {
+        void drawTriangle(const glm::ivec2& p0, const glm::ivec2& p1, const glm::ivec2& p2) const {
             static constexpr std::uint32_t triangleLineColor = 0xFFA78BFA;
             static constexpr std::uint32_t triangleFillColor = 0xFF4C1D95;
             drawFilledTriangle(p0, p1, p2, triangleFillColor);
@@ -121,18 +121,18 @@ namespace rasterizer {
         // Based on diagram by: Pikuma (Gustavo Pezzi)
         //
         ///////////////////////////////////////////////////////////////////////////////
-        void drawFilledTriangle(glm::vec2 p0, glm::vec2 p1, glm::vec2 p2, const std::uint32_t color) const {
+        void drawFilledTriangle(glm::ivec2 p0, glm::ivec2 p1, glm::ivec2 p2, const std::uint32_t color) const {
             sortAscendingVertically(p0, p1, p2);
 
-            if (std::fabs(p1.y - p2.y) < std::numeric_limits<glm::float32_t>::epsilon()) {
+            if (p1.y == p2.y) {
                 // Triangle is flat bottom
                 drawFlatBottomTriangle(p0, p1, p2, color);
-            } else if (std::fabs(p0.y - p1.y) < std::numeric_limits<glm::float32_t>::epsilon()) {
+            } else if (p0.y == p1.y) {
                 // Triangle is flat top
                 drawFlatTopTriangle(p0, p1, p2, color);
             } else {
                 // Solution based on similar triangles
-                const glm::vec2 mid{
+                const glm::ivec2 mid{
                     (p2.x - p0.x) * (p1.y - p0.y) / (p2.y - p0.y) + p0.x,
                     p1.y
                 };
@@ -163,21 +163,20 @@ namespace rasterizer {
         // Based on diagram by: Pikuma (Gustavo Pezzi)
         //
         ///////////////////////////////////////////////////////////////////////////////
-        void drawFlatBottomTriangle(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2,
+        void drawFlatBottomTriangle(const glm::ivec2& p0, const glm::ivec2& p1, const glm::ivec2& p2,
                                     const std::uint32_t color) const {
             // Find the two slopes (two triangle legs)
             // Inverse is taken as y becomes independent axis
-            const glm::float32_t invSlope01 = (p1.x - p0.x) / (p1.y - p0.y);
-            const glm::float32_t invSlope02 = (p2.x - p0.x) / (p2.y - p0.y);
+            const auto invSlope01 = static_cast<glm::float32_t>(p1.x - p0.x) / static_cast<glm::float32_t>(p1.y - p0.y);
+            const auto invSlope02 = static_cast<glm::float32_t>(p2.x - p0.x) / static_cast<glm::float32_t>(p2.y - p0.y);
 
             // Start at the top vertex p0
-            glm::float32_t startX = p0.x;
-            glm::float32_t endX = p0.x;
+            auto startX = static_cast<glm::float32_t>(p0.x);
+            auto endX = static_cast<glm::float32_t>(p0.x);
 
             // Loop all the scanlines from top to bottom
             // Since Y+ is downward, we increment Y on each iteration
-            const auto maxY = static_cast<std::uint32_t>(p2.y);
-            for (auto y = static_cast<std::uint32_t>(p0.y); y <= maxY; ++y) {
+            for (auto y = p0.y; y <= p2.y; ++y) {
                 drawLine({startX, y}, {endX, y}, color);
                 startX += invSlope01;
                 endX += invSlope02;
@@ -197,21 +196,20 @@ namespace rasterizer {
         // Based on diagram by: Pikuma (Gustavo Pezzi)
         //
         ///////////////////////////////////////////////////////////////////////////////
-        void drawFlatTopTriangle(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2,
+        void drawFlatTopTriangle(const glm::ivec2& p0, const glm::ivec2& p1, const glm::ivec2& p2,
                                  const std::uint32_t color) const {
             // Find the two slopes (two triangle legs)
             // Inverse is taken as y becomes independent axis
-            const glm::float32_t invSlope20 = (p0.x - p2.x) / (p0.y - p2.y);
-            const glm::float32_t invSlope21 = (p1.x - p2.x) / (p1.y - p2.y);
+            const auto invSlope20 = static_cast<glm::float32_t>(p0.x - p2.x) / static_cast<glm::float32_t>(p0.y - p2.y);
+            const auto invSlope21 = static_cast<glm::float32_t>(p1.x - p2.x) / static_cast<glm::float32_t>(p1.y - p2.y);
 
             // Start at the top vertex p0
-            glm::float32_t startX = p2.x;
-            glm::float32_t endX = p2.x;
+            auto startX = static_cast<glm::float32_t>(p2.x);
+            auto endX = static_cast<glm::float32_t>(p2.x);
 
             // Loop all the scanlines from top to bottom
             // Since Y- is upward, we decrement Y on each iteration
-            const auto minY = static_cast<std::uint32_t>(p0.y);
-            for (auto y = static_cast<std::uint32_t>(p2.y); y >= minY; --y) {
+            for (auto y = p2.y; y >= p0.y; --y) {
                 drawLine({startX, y}, {endX, y}, color);
                 startX -= invSlope20;
                 endX -= invSlope21;
@@ -250,7 +248,7 @@ namespace rasterizer {
             return static_cast<std::uint32_t*>(std::calloc(width * height, sizeof(std::uint32_t)));
         }
 
-        static void sortAscendingVertically(glm::vec2& p0, glm::vec2& p1, glm::vec2& p2) {
+        static void sortAscendingVertically(glm::ivec2& p0, glm::ivec2& p1, glm::ivec2& p2) {
             if (p1.y < p0.y) {
                 // p1.y < p0.y => p0.y < p1.y
                 std::swap(p0, p1);
